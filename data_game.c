f603a540891ce0e95aee1c41476c60a7b45b0ae4
flow-J <hongjunjie2012@yeah.net>
2015-12-04 02:13:09 +0800

    code

--- data_game.c
+++ data_game.c
@@ -1,3 +1,4 @@
+/****************
 #include <stdio.h>
 struct queue
 {
@@ -17,6 +18,7 @@ int main()
     struct queue q1,q2;
     struct stack s;
     int book[10];
+    int i,t;
 
     q1.head=1; q1.tail=1;
     q2.head=1; q2.tail=1;
@@ -53,12 +55,414 @@ int main()
             q1.head++;
             q1.data[q1.tail]=t;
             q1.tail++;
-            while
+            while(s.data[s.top]!=t)
+            {
+                book[s.data[s.top]]=0;
+                q1.data[q1.tail]=s.data[s.top];
+                q1.tail++;
+                s.top--;
+            }
         
+        }
+
+        t=q2.data[q2.head];
+
+        if(book[t]==0)
+        {
+            q2.head++;
+            s.top++;
+            s.data[s.top]=t;
+            book[t]=1;
+        }
+        else
+        {
+            q2.head++;
+            q2.data[q2.tail]=t;
+            q2.tail++;
+            while(s.data[s.top]!=t)
+            {
+                book[s.data[s.top]]=0;
+                q2.data[q2.tail]=s.data[s.top];
+                q2.tail++;
+                s.top--;
+            }
         }
     }
 
+    if(q2.head==q2.tail)
+    {
+        printf("HengWin\n");
+        printf("Heng_car_now_is");
+        for(i=1;i<=s.top;i++)
+            printf(" %d",s.data[i]);
+    }
+    else
+    {
+        printf("HaWin\n");
+        printf("Ha's card is");
+        for(i=q2.head;i<=q2.tail-1;i++)
+            printf(" %d",q2.data[i]);
+        if(s.top>0)
+        {
+            printf("\nthe card on the table is ");
+            for(i=1;i<=s.top;i++)
+                printf(" %d",s.data[i]);
+        }
+        else
+            printf("\n there is no card");
+
+    }
+
+    getchar();getchar();
+    return 0;
+
 }
+
+
+
+
+*/
+
+
+/*
+#include <stdio.h>
+int main()
+{
+    char a[20][21];
+    int i, j,sum,map=0,p,q,x,y,n,m;
+
+    scanf("%d %d",&n,&m);
+
+    for(i=0;i<=n-1;i++)
+        scanf("%s",a[i]);
+
+    //double for to find point in the map
+    for(i=0;i<=n-1;i++)
+    {
+        for(j=0;j<=m-1;j++)
+        {
+            //make suer the point is not a ground, only ground can have a boom.
+            if(a[i][j]=='.')
+            {
+                sum=0;
+
+                x=i; y=j;
+                while(a[x][y]=='G')
+                    sum++;
+                x--;
+
+            
+
+                x=i; y=j;
+                while(a[x][y]!='#')
+                {
+                    if(a[x][y]=='G')
+                        sum++;
+                    x++;
+                }
+                
+                x=i; y=j;
+                while(a[x][y]!='#')
+                {
+                    if(a[x][y]=='G')
+                        sum++;
+                    y--;
+                }
+    
+                x=i; y=j;
+                while(a[x][y]!='#')
+                {
+                    if(a[x][y]=='G')
+                        sum++;
+                    y++;
+                }
+
+               if(sum>map)
+                {
+                    map=sum;
+                    p=i;
+                    q=j;
+                }
+            }
+        }
+    }
+
+    printf("drop the boom on (%d,%d), can kill %d people\n",p,q,map);
+    getchar();getchar();
+    return 0;
+}
+
+*/
+
+
+/*
+
+
+
+
+
+#include <stdio.h>
+int fun(int x)
+{
+    int num=0;
+    int f[10]={6,2,5,5,4,5,6,3,7,6};
+
+    while(x/10!=0)
+    {
+        num += f[x%10];
+        x = x/10;
+    }
+
+    num += f[x];
+    return num;
+}
+
+int main()
+{
+    int a,b,c,m,i,sum=0;
+    scanf("%d",&m);
+
+    for(a=0;a<=1111;a++)
+    {
+        for(b=0;b<=1111;b++)
+        {
+            c=a+b;
+
+            if(fun(a)+fun(b)+fun(c)==m-4)
+            {
+                printf("%d+%d=%d\n", a,b,c);
+                sum++;
+            }
+        }
+    }
+
+    printf("can spell out %d different equation", sum);
+    getchar();getchar();
+    return 0;
+}
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+int a[10],book[10],n;
+
+void dfs(int step)
+{
+    int i;
+    if(step==n+1)
+    {
+        for(i=1;i<=n;i++)
+            printf("%d",a[i]);
+        printf("\n");
+
+        return;
+    }
+
+    for(i=1;i<=n;i++)
+    {
+        if(book[i]==0)
+        {
+            a[step]=i;
+            book[i]=1;
+
+            dfs(step+1);
+            book[i]=0;
+
+
+        }
+    }
+    return;
+}
+
+int main()
+{
+    scanf("%d",&n);
+    dfs(1);
+    getchar();getchar();
+    return 0;
+}
+
+*/
+
+/*
+
+#include <stdio.h>
+int a[10],book[10],total=0;
+void dfs(int step)
+{
+    int i;
+    if(step==10)
+    {
+        if(a[1]*100+a[2]*10+a[3]+a[4]*100+a[5]*10+a[6]
+                ==a[7]*100+a[8]*10+a[9])
+        {
+            total++;
+            printf("%d%d%d+%d%d%d=%d%d%d\n",
+                    a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],a[9]);
+        }
+        return;
+
+    }
+
+    for(i=1;i<=9;i++)
+    {
+        if(book[i]==0)
+        {
+            a[step]=i;
+            book[i]=1;
+
+            dfs(step+1);
+
+            book[i]=0;
+        }
+    }
+    return;
+}
+
+int main()
+{
+    
+    dfs(1);
+
+    printf("total=%d",total/2);
+    getchar();getchar();
+    return 0;
+
+}
+*/
+
+
+
+#include <stdio.h>
+int n,m,p,q,min=99999999;
+int a[51][51],book[51][51];
+void dfs(int x,int y,int step)
+{
+    int next[4][2] = {{ 0, 1},//right
+                        { 1, 0},//down
+                        { 0, -1},//left
+                        {-1, 0}};//up
+
+    int tx,ty,k;
+
+    if(x==p && y==q)
+    {
+        if(step<min)
+            min=step;
+        return;
+    }
+
+    for(k=0;k<=3;k++)
+    {
+        tx=x+next[k][0];
+        ty=y+next[k][1];
+        
+        if(tx<1 || tx>n || ty<1 || ty>m)
+            continue;
+
+        if(a[tx][ty]==0 && book[tx][ty]==0)
+        {
+            book[tx][ty]=1;
+            dfs(tx,ty,step+1);
+            book[tx][ty]=0;
+        }
+
+    }
+    return ;
+
+}
+
+int main()
+{
+    int i,j,startx,starty;
+    scanf("%d %d",&n,&m);//n = x, m = y;
+
+    //read in map
+    for(i=1;i<=n;i++)
+        for(j=1;j<=m;j++)
+            scanf("%d",&a[i][j]); 
+
+    scanf("%d %d %d %d",&startx,&starty,&p,&q);
+
+    book[startx][startx]=1;//the first 
+
+    dfs(startx,starty,0);
+    printf("%d", min);
+    getchar();getchar();
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- datastruct.c
+++ datastruct.c
@@ -127,7 +127,7 @@ int main()
 }
 */
 
-
+/*
 
 #include <stdio.h>
 #include <string.h>
@@ -166,6 +166,290 @@ int main()
     getchar();getchar();
     return 0;
 }
+
+*/
+
+
+
+
+/*
+
+#include <stdio.h>
+int main()
+{
+    int a=10;
+    int *p;
+    p=&a;
+    printf("%d",*p);
+
+    getchar();getchar();
+    return 0;
+}
+
+*/
+
+
+
+
+
+
+/*
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int *p;
+    p=(int *)malloc(sizeof(int));
+    *p=10;
+    printf("%d",*p);
+
+    getchar();getchar();
+}
+
+*/
+
+
+/*
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+int main()
+{
+    struct node *head,*p,*q,*t;
+    int i,n,a;
+    scanf("%d",&n);
+    head = NULL;
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a);
+
+        p=(struct node *)malloc(sizeof(struct node));
+        p->data=a;
+        p->next=NULL;
+        if(head==NULL)
+            head=p;
+        else
+            q->next=p;
+
+        q=p;
+    }
+
+    t=head;
+    while(t !=NULL)
+    {
+        printf("%d ",t->data);
+        t=t->next;
+    }
+
+    getchar();getchar;
+    return 0;
+}
+
+*/
+
+
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+int main()
+{
+    struct node *head,*p,*q,*t;
+    int i,n,a;
+    scanf("%d",&n);
+    head = NULL;
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a);
+        p=(struct node *)malloc(sizeof(struct node));
+        p->data=a;
+        p->next=NULL;
+        if(head=NULL)
+            head=p;
+        else
+            q->next=p;
+
+        q=p;
+    }
+
+    scanf("%d", &a);
+    t=head;
+    while(t !=NULL)
+    {
+        if(t->next->data > a)
+        {
+            p=(struct node *)malloc(sizeof(struct node));
+
+            p->data=a;
+            p->next=t->next;
+            t->next=p;
+            break;
+        }
+        t=t->next;
+    }
+
+    t=head;
+    while(t !=NULL)
+    {
+        printf("%d ",t->data);
+        t=t->next;
+
+    }
+
+    getchar();getchar();
+    return 0;
+
+}
+*/
+
+#include <stdio.h>
+int main()
+{
+    int data[101],right[101];
+    int i,n,t,len;
+    
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+        scanf("%d",&data[i]);
+    len=n;
+
+    for(i=1;i<=n;i++)
+    {
+        if(i!=n)
+            right[i]=i+1;
+        else
+            right[i]=0;
+
+    }
+
+    len++;
+    scanf("%d",&data[len]);
+
+    t=1;
+    while(t!=0)
+    {
+        if(data[right[t]]>data[len])
+
+        {
+            right[len]=right[t];
+
+            right[t]=len;
+            break;
+        }
+        t=right[t];
+
+    }
+
+    t=1;
+    while(t!=0)
+    {
+        printf("%d ",data[t]);
+        t=right[t];
+
+    }
+    getchar();
+    getchar();
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
