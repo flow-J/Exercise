873da301a4ee07222ab191ceccf0e4b0c8b193d3
flow-J <hongjunjie2012@yeah.net>
2015-12-13 16:40:08 +0800

    lisp

--- lispy.py
+++ lispy.py
@@ -1,13 +1,44 @@
 # coding: utf-8
+from __future__ import division
+
+Symbol = str
+# 字符串函数 把收到的所有数据类型都变成字符串
+
+class Env(dict):
+    def __init__(self, parms=(), args=(), outer=None):
+        self.update(zip(parms,args))
+        self.outer = outer
+    def find(self, var):
+        "用来为一个变量查找正确的环境"
+        return self if var in self else self.outer.find(var)
+
+
+
+
+def add_globals(env):
+    "添加 20 种 常用的过程"
+    import math, operator as op
+    env.update(vars(math)) # 数学相关
+    env.update(
+        {'+':op.add, '-':op.sub, '*':op.mul, '/':op.div, 'not':op.not_,
+         '>':op.gt, '<':op.lt, '>=':op.ge, '<=':op.le, '=':op.eq,
+         'equal?':op.eq, 'eq?':op.is_, 'length':len, 'cons':lambda x,y:[x]+y,
+         'car':lambda x:x[0], 'cdr':lambda x:x[1:], 'append':op.add,
+         'list':lambda *x:list(x), 'list?':lambda x:isa(x,list),
+         'null>':lambda x:x==[], 'symbol?':lambda x: isa(x,Symbol)})
+    return env
+
+global_env = add_globals(Env())
+
+isa = isinstance
+
 
 
 def eval(x, env=global_env):
     "评估一个表达式的环境."
-    if isa(x, Symbol):
-# 变量引用
+    if isa(x, Symbol): # 变量引用
         return env.find(x)[x]
-    elif not isa(x, list):
-# 字面常量
+    elif not isa(x, list): # 字面常量
         return x
     elif x[0] == 'quote':
         (_, exp) = x
@@ -34,28 +65,6 @@ def eval(x, env=global_env):
         return proc(*exps)
 
 
-class Env(dict):
-    def __init__(self, parms=(), args=(), outer=None):
-        self.update(zip(parms,args))
-        self.outer = outer
-    def find(self, var):
-        "用来为一个变量查找正确的环境"
-
-
-
-def add_globals(env):
-    "添加 20 种 常用的过程"
-    import math, operator as op
-    env.update(vars(math))
-# 数学相关
-    env.update(
-        {'+':op.add, '-':op.sub, '*':op.mul, '/':op.div, 'not':op.not_,
-         '>':op.gt, '<':op.lt, '>=':op.ge, '<=':op.le, '=':op.eq,
-         'equal?':op.eq, 'eq?':op.is_, 'length':len, 'cons':lambda x,y:[x]+y,
-         'car':lambda x:x[0], 'cdr':lambda x:x[1:], 'append':op.add,
-         'list':lambda *x:list(x), 'list?':lambda x:isa(x,list),
-         'null>':lambda x:x==[], 'symbol?':lambda x: isa(x,Symbol)})
-    return env
 
 
 
@@ -66,7 +75,7 @@ def read(s):
 parse = read
 
 def tokenize(s):
-    return s.replace('(','(').replace(')',')').split()
+    return s.replace('(',' ( ').replace(')',' ) ').split()
 
 
 def read_from(tokens):
@@ -77,7 +86,7 @@ def read_from(tokens):
         L = []
         while tokens[0] != ')': # 当碰见）右括号的时候 结束循环
             L.append(read_from(tokens))
-            tokens.pop(0)
+            tokens.pop(0) # pop off
             return L
     elif ')' == token:
         raise SyntaxError('unexpected )')
