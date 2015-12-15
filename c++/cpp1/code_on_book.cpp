79ee707aa6eaf6c33185cf381e6303a67758bde6
flow-J <hongjunjie2012@yeah.net>
2015-12-16 00:01:44 +0800

    code

--- c++/cpp1/code_on_book.cpp
+++ c++/cpp1/code_on_book.cpp
@@ -32,7 +32,7 @@ int main()
     std::cout << "Sum is: " << sum << std::endl;
     return 0;
 }*/
-
+/*
 #include <iostream>
 int main()
 {
@@ -55,4 +55,44 @@ int main()
             << cnt << " times" << std::endl;
     }
     return 0;
+}*/
+
+/*
+#include <iostream>
+#include "Sales_item.h"
+int main()
+{
+    Sales_item book;
+    
+    std::cin >> book;
+    std::cout << book << std::endl;
+    return 0;
+}*/
+/*
+#include <iostream>
+#include "Sales_item.h"
+int main()
+{
+    Sales_item item1, item2;
+    std::cin >> item1 >> item2;
+    std::cout << item1 + item2 << std::endl;
+    return 0;
+}*/
+
+#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+    Sales_item item1, item2;
+    std::cin >> item1 >> item2;
+
+    if (item1.isbn() == item2.isbn()) {
+        std::cout << item1 + item2 << std::endl;
+        return 0;
+    } else {
+        std::cerr << "Data mush refer to same ISB"
+            << std::endl;
+        return -1;
+    }
 }
