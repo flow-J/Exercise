a9ff9c0d59d4af2528c6b8fc0efb00abeef37fec
flow <hongjunjie2012@yeah.net>
2015-12-15 20:02:47 +0800

    Update ex1.10.cpp

--- c++/cpp1/ex1.10.cpp
+++ c++/cpp1/ex1.10.cpp
@@ -3,6 +3,6 @@ int main()
 {
     int foo = 10;
     while(foo >= 0)
-        std::cout << foo-- << " " << std::endl;
+        std::cout << foo-- << " " << std::endl; //foo--先调用后运算，如果是--foo的话，output就是9....-1啦。
     return 0;
 }
