b863da2654cf48bc4751da13467c54bf114fbd2b
flow-J <hongjunjie2012@yeah.net>
2015-12-16 23:04:15 +0800

    sale data

---
+++ c++/cpp_2/Sale_data.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+struct Sales_data
+{
+    std::string bookNO;
+    std::string bookName;
+    unsigned units_sold = 0;
+    double price = 0.0;
+    double revenue = 0.0;
+};
+
+int main()
+{
+    Sales_data data1, data2;
+    double price = 0; // book price
+    std::cin >> data1.bookNo >> data1.units_sold >> price;
+    data1.revenue = data1.units_sold * price;
+}
