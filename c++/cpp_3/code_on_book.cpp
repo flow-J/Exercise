e64f93277894ae10f41bbcaabf302866fb8f0216
flow-J <hongjunjie2012@yeah.net>
2015-12-19 16:25:47 +0800

    test

---
+++ c++/cpp_3/code_on_book.cpp
@@ -0,0 +1,70 @@
+/*
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+
+int main()
+{
+    string s;
+    cin >> s;
+    cout << s << endl;
+    return 0;
+}*/
+/*
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+int main()
+{
+    string word;
+    while (cin >> word)
+        cout << word << endl;
+    return 0;
+}*/
+
+
+
+/*
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+
+int main()
+{
+    string line;
+
+    while (getline(cin, line))
+        cout << line << endl;
+    return 0;
+}*/
+
+/*
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+
+int main()
+{
+string line;
+while (getline(cin, line))
+    if (line.size() > 80)
+        cout << line << endl;
+}
+*/
