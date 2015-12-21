1831c64fe52ed1e6249c1f6b7328e27b702c60ef
flow-J <hongjunjie2012@yeah.net>
2015-12-22 05:47:47 +0800

    code

---
+++ c++/cpp_4/ex4.11.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int a, b, c, d;
+
+int main()
+{
+    while (cin >> a >> b >> c >> d)
+        if (a > b && b > c && c >> d)
+            cout << "a > b > c > d" << endl;
+}
