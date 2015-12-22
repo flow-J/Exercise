37393b7f422b939f9d9e332f608b5ccaa34751ce
flow <hongjunjie2012@yeah.net>
2015-12-23 02:57:06 +0800

    Update ex4.28.cpp

--- c++/cpp_4/ex4.28.cpp
+++ c++/cpp_4/ex4.28.cpp
@@ -5,5 +5,5 @@ using std::endl;
 
 int main()
 {
-    cout << "void: nullptr_t\t" << sizeof(std::nullptr_t) << " bytes" << endl << endl;
+    cout << "void: nullptr_t\t" << sizeof(std::nullptr_t) << " bytes" << endl << endl; // 多了一给endl 多了一行空格。
 }
