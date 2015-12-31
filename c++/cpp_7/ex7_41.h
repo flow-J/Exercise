a194f9b70c86e3ba433dfe523b89845d7c49f76e
flow <hongjunjie2012@yeah.net>
2015-12-31 15:43:04 +0800

    Update ex7_41.h

--- c++/cpp_7/ex7_41.h
+++ c++/cpp_7/ex7_41.h
@@ -1,3 +1,4 @@
+
 #ifndef CP5_ex7_41_h
 #define CP5_ex7_41_h
 
@@ -11,21 +12,21 @@ class Sales_data {
 
 public:
     Sales_data(const std::string &s, unsigned n, double p):bookNo(s), units_sold(n), revenue(n*p)
-    { std::cout << "Sales_data(const std::String&, unsigned, double)" << std::endl; }
-
+    { std::cout << "Sales_data(const std::string&, unsigned, double)" << std::endl; }
+    
     Sales_data() : Sales_data("", 0, 0.0f)
-    { std::cout << "Sales_data() "<< std::endl; }
-
+    { std::cout << "Sales_data()" << std::endl; }
+    
     Sales_data(const std::string &s) : Sales_data(s, 0, 0.0f)
     { std::cout << "Sales_data(const std::string&)" << std::endl; }
-
+    
     Sales_data(std::istream &is);
 
     std::string isbn() const { return bookNo; }
     Sales_data& combine(const Sales_data&);
-
+    
 private:
-    inline double avg_price() const;
+    inline double avg_price() const;        
 
 private:
     std::string bookNo;
@@ -39,9 +40,9 @@ double Sales_data::avg_price() const
     return units_sold ? revenue/units_sold : 0;
 }
 
-
+// declarations for nonmember parts of the Sales_data interface.
 std::istream &read(std::istream &is, Sales_data &item);
-std::ostream &print(std::ostream &os,const Sales_data &item);
-Sales_data add(const std::istream &lhs,const Sales_data &rhs);
+std::ostream &print(std::ostream &os, const Sales_data &item);
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs);
 
 #endif
