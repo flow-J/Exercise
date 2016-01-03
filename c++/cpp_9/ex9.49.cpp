36ed695201b31863e8b95742d81f45dca90723b1
flow-J <hongjunjie2012@yeah.net>
2016-01-03 10:43:33 +0800

    .

--- c++/cpp_9/ex9.49.cpp
+++ c++/cpp_9/ex9.49.cpp
@@ -17,5 +17,9 @@ int main()
     {
         if (string::npos == curr.find_first_not_of("aceimnorsuvwxz"))
             longest = longest.size() < curr.size() ? curr : longest;
-    }
+    };
+    for (string curr; ifs >> curr; updata_with(curr));
+    cout << longest << endl;
+
+    return 0;
 }
