6ac44677f7cce70898506d7b62c8db9aa948f435
flow <hongjunjie2012@yeah.net>
2016-01-04 19:16:49 +0800

    Update ex10.33.cpp

--- c++/cpp_10/ex10.33.cpp
+++ c++/cpp_10/ex10.33.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
+
+#include <fstream>
 #include <iterator>
 #include <algorithm>
 
-int main(int argc,char **avgv)
+int main(int argc, char **argv)
 {
     if (argc != 4) return -1;
 
@@ -12,7 +13,9 @@ int main(int argc,char **avgv)
     std::istream_iterator<int> in(ifs), in_eof;
     std::ostream_iterator<int> out_odd(ofs_odd, " "), out_even(ofs_even, "\n");
 
-    std::for_each(in, in_eof, [%out_odd, &out_even](const int i){
-            *(i & 0x1 ? out_odd : out_even)++ = i;
-            });
+    std::for_each(in, in_eof, [&out_odd, &out_even](const int i){
+        *(i & 0x1 ? out_odd : out_even)++ = i;
+    });
+
+    return 0;
 }
