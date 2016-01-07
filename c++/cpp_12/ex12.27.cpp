de2a06bb906bb8d67653028b6d825816e912d16a
flow <hongjunjie2012@yeah.net>
2016-01-08 01:52:50 +0800

    Update ex12.27.cpp

--- c++/cpp_12/ex12.27.cpp
+++ c++/cpp_12/ex12.27.cpp
@@ -4,17 +4,16 @@
 void runQueries(std::ifstream &infile)
 {
     TextQuery tq(infile);
-    while (true)
-    {
+    while (true) {
         std::cout << "enter word to look for, or q to quit: ";
         string s;
-        if (!(std::cin >> s) || s == 'q') break;
+        if (!(std::cin >> s) || s == "q") break;
         print(std::cout, tq.query(s)) << std::endl;
     }
 }
 
 int main()
 {
-    std::ifstream file("../book/stroyDataFile.txt");
+    std::ifstream file("../book/storyDataFile.txt");
     runQueries(file);
 }
