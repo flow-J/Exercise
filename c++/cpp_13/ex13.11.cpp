e1e78318725bfefe1f0a51be62ac29cf2343f62b
flow-J <hongjunjie2012@yeah.net>
2016-01-09 20:52:15 +0800

    ,

---
+++ c++/cpp_13/ex13.11.cpp
@@ -0,0 +1,18 @@
+class HasPtr {
+public:
+    HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) { }
+    HasPtr(const HasPtr &hp) : ps(new std::string(*hp.ps)),i(hp.i) { }
+    HasPtr& operator=(const HasPtr &hp) {
+        string *new_ps = new string(*hp.ps);
+        delete ps;
+        ps = new_ps;
+        i = hp.i;
+        return *this;
+    }
+    ~HasPtr() {
+        delete ps;
+    }
+private:
+    std::string *ps;
+    int i;
+};
