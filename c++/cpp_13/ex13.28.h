d7a7989332df56bbc72acbbd5960dd4344294a00
flow-J <hongjunjie2012@yeah.net>
2016-01-11 03:09:30 +0800

    .

---
+++ c++/cpp_13/ex13.28.h
@@ -0,0 +1,11 @@
+#ifndef CP5_ex13_28_h
+#define CP5_ex13_28_h
+
+#include <string>
+using std::string;
+
+class TreeNode {
+public:
+    TreeNode() : value(string()),
+
+};
---
+++ c++/cpp_13/ex13.30.h
@@ -0,0 +1,7 @@
+void swap(HasPtr& lhs, HasPtr& rhs)
+{
+    using std::swap;
+    swap(lhs.ps, rhs.ps);
+    swap(lhs.i, rhs.i);
+    std::cout << "call swap(HasPtr& lhs, HasPtr& rhs)" << std::endl;
+}
