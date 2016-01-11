0ac611dcf6124a793df5612ae8788cec2ccbff3e
flow-J <hongjunjie2012@yeah.net>
2016-01-11 23:21:42 +0800

    bug

---
+++ c++/cpp_14/ex14.5.h
@@ -0,0 +1,52 @@
+#ifndef CP5_ex14_5_h
+#define CP5_ex14_5_h
+
+#include <iostream>
+#include <string>
+
+class Book {
+    friend std::istream& operator>>(std::istream&, Book&);
+    friend std::ostream& operator<<(std::ostream&, const Book&);
+    friend bool operator==(const Book&, const Book&);
+    friend bool operator!=(const Book&, const Book&);
+
+public:
+    Book() = default;
+    Book(unsigned no, std::string name, std::string author, std::string pubdate):no_(no), name_(name), author_(author), pubdate_(pubdate) { }
+    Book(std::istream &in) { in >> *this; }
+
+private:
+    unsigned no_;
+    std::string name_;
+    std::string author_;
+    std::string pubdate_;
+};
+
+std::istream& operator>>(std::istream&, Book&);
+std::ostream& operator<<(std::ostream&, const Book&);
+bool operator==(const Book&, const Book&);
+bool operator!=(const Book&, const Book&);
+
+#endif
+
+std::istream& operator>>(std::istream &in, Book &book)
+{
+    in >> book.no_ >>book.name_ >> book.author_ >> book.pubdate_;
+    return in;
+}
+
+std::ostream& operator<<(const Book &out, const Book &book)
+{
+    out << book.no_ << " " << book.name_ << " " << book.author_ << " " << book.pubdate_;
+    return out;
+}
+
+bool operator==(const Book &lhs, const Book &rhs)
+{
+    return lhs.no_ == rhs.no_;
+}
+
+bool operator!=(const Book &lhs, const Book &rhs)
+{
+    return !(lhs == rhs);
+}
