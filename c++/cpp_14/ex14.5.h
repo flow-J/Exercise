3a577e9a5c9b45b6b3ecf87c3c9951ac4292059f
flow <hongjunjie2012@yeah.net>
2016-01-11 23:23:34 +0800

    .
    ..

--- c++/cpp_14/ex14.5.h
+++ c++/cpp_14/ex14.5.h
@@ -31,11 +31,11 @@ bool operator!=(const Book&, const Book&);
 
 std::istream& operator>>(std::istream &in, Book &book)
 {
-    in >> book.no_ >>book.name_ >> book.author_ >> book.pubdate_;
+    in >> book.no_ >> book.name_ >> book.author_ >> book.pubdate_;
     return in;
 }
 
-std::ostream& operator<<(const Book &out, const Book &book)
+std::ostream& operator<<(std::ostream &out, const Book &book)
 {
     out << book.no_ << " " << book.name_ << " " << book.author_ << " " << book.pubdate_;
     return out;
@@ -50,3 +50,4 @@ bool operator!=(const Book &lhs, const Book &rhs)
 {
     return !(lhs == rhs);
 }
+
