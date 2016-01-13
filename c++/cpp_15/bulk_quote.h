744579bb79dcdd870302e72c542c5ea58afa51a4
flow-J <hongjunjie2012@yeah.net>
2016-01-14 03:22:40 +0800

    .

--- c++/cpp_15/bulk_quote.h
+++ c++/cpp_15/bulk_quote.h
@@ -1,14 +1,44 @@
 #ifndef BULK_QUOTE_H
 #define BULK_QUOTE_H
+#include "disc_quote.h"
+
 
 class Bulk_quote : public Disc_quote
 {
 public:
-    Bulk_quote() = default;
-    Bulk_quote(const std::string& b, double p, std::size_t q, double disc) : Disc_quote(b, p, q, disc) { }
+    Bulk_quote() { std::cout << "default constructing Bulk_quote\n"; }
+    using Disc_quote::Disc_quote;
+
+    Bulk_quote(const Bulk_quote& bq) : Disc_quote(bq)
+    { std::cout << "Bulk_quote : copy constructor\n"; }
+    
+    Bulk_quote(Bulk_quote&& bq) : Disc_quote(std::move(bq))
+    {
+        std::cout << "Bulk_quote : move constructor\n";
+    }
+
+    Bulk_quote& operator =(const Bulk_quote& rhs)
+    {
+        Disc_quote::operator =(rhs);
+        std::cout << "Bulk_quote : copy=()\n";
+
+        return *this;
+    }
+
+    Bulk_quote& operator =(Bulk_quote&& rhs)
+    {
+        Disc_quote::operator =(std::move(rhs));
+        std::cout << "Bulk_quote : move=()\n";
+        return *this;
+    }
 
     double net_price(std::size_t n) const override;
     void   debug() const override;
+
+    ~Bulk_quot() override
+    {
+        std::cout << "destructing Bulk_quote\n";
+    }
 };
 
 #endif
