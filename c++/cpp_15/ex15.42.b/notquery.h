c7f99fd8916b165b8b2069e2267aa07cd5c05e6d
flow <hongjunjie2012@yeah.net>
2016-01-15 04:10:45 +0800

    Update notquery.h

--- c++/cpp_15/ex15.42.b/notquery.h
+++ c++/cpp_15/ex15.42.b/notquery.h
@@ -15,41 +15,45 @@ class TextQuery;
 
 class NotQuery :public Query_base
 {
-    friend Query operator~(const Query&);
-    NotQuery(const Query &q) :query(q){ }
-    string rep() const { return "~(" + query.rep() + ")"; }
-    QueryResult eval(const TextQuery&) const;
+	friend Query operator~(const Query&);
+	//call Query's default copy constructor.
+	NotQuery(const Query &q) :query(q){ }
+	string rep() const{ return "~(" + query.rep() + ")"; }
+	QueryResult eval(const TextQuery&) const;
 
-    Query query;
+	Query query;
 };
 
 inline Query operator~(const Query &operand)
 {
-    return shared_ptr<Query_base>(new NotQuery(operand));
+	return shared_ptr<Query_base>(new NotQuery(operand));
 }
 
 #endif
 
-class "textquery.h"
-class "queryResult.h"
+
+#include "queryresult.h"
+#include "textquery.h"
+#include "query.h"
+
 
 QueryResult
-NotResult::eval(const TextQuery &text) const
+NotQuery::eval(const TextQuery &text) const
 {
-    auto result = query.eval(text);
+	auto result = query.eval(text);
 
-    auto ret_lines = make_shared<set<line_no>>();
+	auto ret_lines = make_shared<set<line_no>>();
 
-    auto beg = result.begin();
-    auto end = result.end();
+	auto beg = result.begin();
+	auto end = result.end();
 
-    auto sz = result.getfile()->size();
-    for (size_t n = 0; n != sz; ++n)
-    {
-        if (beg == end || *beg != n)
-            ret_lines->insert(n);
-        else if (beg != end)
-            ++beg;
-    }
-    return QueryResult(rep(), ret_lines, result.get_file());
+	auto sz = result.get_file()->size();
+	for (size_t n = 0; n != sz; ++n)
+	{
+		if (beg == end || *beg != n)
+			ret_lines->insert(n);
+		else if (beg != end)
+			++beg;
+	}
+	return QueryResult(rep(), ret_lines, result.get_file());
 }
