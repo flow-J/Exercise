ba1c67ec1a3c363a9fbe9d0ad3c860dfe959c0d9
flow <hongjunjie2012@yeah.net>
2016-01-15 04:08:58 +0800

    Update andquery.h

--- c++/cpp_15/ex15.42.b/andquery.h
+++ c++/cpp_15/ex15.42.b/andquery.h
@@ -10,19 +10,20 @@ using std::shared_ptr;
 class QueryResult;
 class TextQuery;
 
-class AndQuery :publc BinaryQuery
+class AndQuery :public BinaryQuery
 {
-    friend Query operator&(const Query&, const Query&);
-    AndQuery(const Query &left, const Query &right) :BinaryQuery(left, right, "&"){ }
-    QueryResult eval(const TextQuery&) const;
+	friend Query operator&(const Query&, const Query&);
+	AndQuery(const Query &left, const Query &right) :BinaryQuery(left, right, "&"){ }
+	QueryResult eval(const TextQuery&) const;
 };
 
 inline Query operator&(const Query &lhs, const Query &rhs)
 {
-    return shared_ptr<Query_base>(new AndQuery(lhs, rhs));
+	return shared_ptr<Query_base>(new AndQuery(lhs, rhs));
 }
 
 #endif
+
 
 #include <algorithm>
 using std::set_intersection;
@@ -37,15 +38,16 @@ using std::set;
 #include "textquery.h"
 #include "query.h"
 
+
 QueryResult
 AndQuery::eval(const TextQuery& text) const
 {
-    auto left = lhs.eval(text);
-    auto right = rhs.eval(text);
+	auto left = lhs.eval(text);
+	auto right = rhs.eval(text);
 
-    auto ret_lines = make_shared<set<line_no>> ();
+	auto ret_lines = make_shared<set<line_no>>();
 
-    set_intersection(left.begin(), left.end(), right.begin(), right.end(), inserter(*ret_lines, ret_lines->begin()));
+	set_intersection(left.begin(), left.end(), right.begin(), right.end(), inserter(*ret_lines, ret_lines->begin()));
 
-    return QueryResult(rep(), ret_lines, left.get_file());
+	return QueryResult(rep(), ret_lines, left.get_file());
 }
