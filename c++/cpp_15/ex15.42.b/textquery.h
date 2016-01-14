9786f3f11e7c7cfbb90ad8cdb6ea0cab9ccd8da1
flow <hongjunjie2012@yeah.net>
2016-01-15 04:04:51 +0800

    Update textquery.h

--- c++/cpp_15/ex15.42.b/textquery.h
+++ c++/cpp_15/ex15.42.b/textquery.h
@@ -43,37 +43,12 @@ private:
 #include <sstream>
 using std::istringstream;
 
-TextQuery::TextQuery(ifstream& is) :file(new vector<string>)
-{
-        using std::getline;
-
-        string text;
-    while (getline(is, text))
-    {
-        file->push_back(text);
-            int n = file->size() - 1;
-            istringstream line(text);
-        string word;
-        while (line >> word)
-        {
-            auto p = handlePunct(word);
-            for (auto w : *p)
-            {
-                auto &lines = wm[w];
-                if (!lines)
-                    lines.reset(new set<line_no>);
-                lines->insert(n);
-            }
-        }
-    }
-
-}
-
-
 QueryResult TextQuery::query(const string &sought) const
 {
     static shared_ptr<set<line_no>> nodata(new set<line_no>);
 
+    // fetch the iterator to the matching element in the map<word, lines>.
+    //std::map<std::string, std::shared_ptr<std::set<line_no>>>::const_iterator
     auto loc = wm.find(sought);
     if(loc == wm.end())
         return QueryResult(sought, nodata, file);
@@ -83,25 +58,27 @@ QueryResult TextQuery::query(const string &sought) const
 
 shared_ptr<vector<string>> TextQuery::handlePunct(const string &s)
 {
-    shared_prt<vector<string>> p = make_shared<vector<string>>();
-    size_t first = 0, index = 0;
-    while(index != s.size())
-    {
-        if (ispunct(s[index]))
-        {
-            string word = s.substr(first, index - first);
-            if (!word.empty())
-                p->push_back(word);
-            p->push_back(s.substr(index, 1));
-            ++index;
-            first = index;
-        }
-        else
-            ++index;
-    }
-    string trail = s.substr(first);
-    if (!trail.empty())
-        p->push_back(trail);
+	shared_ptr<vector<string>> p =
+		make_shared<vector<string>>();
+	size_t first = 0, index = 0;
+	while(index != s.size())
+	{
+		if (ispunct(s[index]))
+		{
+			string word = s.substr(first, index - first);
+			if (!word.empty())
+				p->push_back(word);
+			p->push_back(s.substr(index, 1));
+			++index;
+			first = index;
+		}
+		else
+			++index;
+	}
+	string trail = s.substr(first);
+	if (!trail.empty())
+		p->push_back(trail);
 
-    return p;
+	return p;
 }
+
