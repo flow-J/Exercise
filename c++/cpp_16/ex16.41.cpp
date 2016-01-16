8aac46b325b665d1c033c0111dfb7748f17731f2
flow-J <hongjunjie2012@yeah.net>
2016-01-16 10:53:08 +0800

    update

---
+++ c++/cpp_16/ex16.41.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+template <typename T>
+auto sum(T lhs, T rhs)->decltype( lhs + rhs)
+{
+    return lhs + rhs;
+}
+
+int main()
+{
+    auto s = sum(122222222222222222222222222, 11111111111111111111111111);
+}
