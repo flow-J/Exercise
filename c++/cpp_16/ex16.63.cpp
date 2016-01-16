e23b5905ceaf23464b7c70ed0caf94a1cb4245bb
flow <hongjunjie2012@yeah.net>
2016-01-17 05:18:12 +0800

    Update ex16.63.cpp

--- c++/cpp_16/ex16.63.cpp
+++ c++/cpp_16/ex16.63.cpp
@@ -1,19 +1,21 @@
+
 #include <iostream>
 #include <vector>
 #include <cstring>
-using std::vector;
 
-template <>
-std::size_t count(vector<const char*> const& vec, const char* value)
+template<>
+std::size_t count (std::vector<const char*> const& vec, const char* value)
 {
     auto count = 0u;
     for(auto const& elem : vec)
-        if (0 == strcmp(value, elem)) ++count;
+        if(0 == strcmp(value, elem)) ++count;
     return count;
 }
-
 int main()
 {
-    std::vector<const char*> vcc = {"alan", "alan", "alan", "alan", "moophy"};
+
+    std::vector<const char*> vcc = { "alan", "alan", "alan", "alan", "moophy" };
     std::cout << count(vcc, "alan") << std::endl;
+
+    return 0;
 }
