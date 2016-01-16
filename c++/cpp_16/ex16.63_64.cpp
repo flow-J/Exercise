9df28e7042708fd7591a6c9e4d18b3da8eaa3235
flow <hongjunjie2012@yeah.net>
2016-01-17 05:24:07 +0800

    Update ex16.63_64.cpp

--- c++/cpp_16/ex16.63_64.cpp
+++ c++/cpp_16/ex16.63_64.cpp
@@ -2,15 +2,17 @@
 #include <vector>
 #include <cstring>
 
+// template
 template<typename T>
-std::size_t count(std::vector<T> const& vec, value)
+std::size_t  count(std::vector<T> const& vec, T value) 
 {
     auto count = 0u;
-    for(auto const& elem : vec)
+    for(auto const& elem  : vec)
         if(value == elem) ++count;
     return count;
 }
 
+// template specialization
 template<>
 std::size_t count (std::vector<const char*> const& vec, const char* value)
 {
@@ -21,7 +23,11 @@ std::size_t count (std::vector<const char*> const& vec, const char* value)
 }
 int main()
 {
-
+    // for ex16.63
+    std::vector<double> vd = { 1.1, 1.1, 2.3, 4 };
+    std::cout << count(vd, 1.1) << std::endl;
+    
+    // for ex16.64
     std::vector<const char*> vcc = { "alan", "alan", "alan", "alan", "moophy" };
     std::cout << count(vcc, "alan") << std::endl;
 
