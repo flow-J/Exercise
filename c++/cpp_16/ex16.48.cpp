6349e6e9b74705574d54749202130dab9a5bf95a
flow-J <hongjunjie2012@yeah.net>
2016-01-16 12:34:36 +0800

    last

--- c++/cpp_16/ex16.48.cpp
+++ c++/cpp_16/ex16.48.cpp
@@ -18,4 +18,31 @@ template <typename T> std::string debug_rep(const T& t)
 }
 
 
-template <typename T>
+template<typename T> std::string debug_rep(T* p)
+{
+    std::ostringstream ret;
+    ret << "pointer: " << p;
+
+    if (p)
+        ret << " " << debug_rep(*p);
+    else
+        ret << " null pointer";
+
+    return ret.str();
+}
+
+std::string debug_rep(const std::string &s)
+{
+    return '"' + s +'"' ;
+}
+
+
+std::string debug_rep(char *p)
+{
+    return debug_rep(std::string(p));
+}
+
+std::string debug_rep(const char *p)
+{
+    return debug_rep(std::string(p));
+}
