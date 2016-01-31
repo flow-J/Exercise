0d271fcd5b9c8252329fa25b88ec18795ef5e810
flow-J <hongjunjie2012@yeah.net>
2016-02-01 02:32:54 +0800

    balblba

--- data_and_Algorith_test/test2.1.cpp
+++ data_and_Algorith_test/test2.1.cpp
@@ -4,9 +4,9 @@
 auto test(int N)
 {
     unsigned i, j, k, count = 0;
-    for (i = 0; i < N; i++)
-        for (j = 0; j < N; j++)
-            for (k = 0; k < N; k++)
+    for (i = 0; i < N; ++i)
+        for (j = 0; j < N; ++j)
+            for (k = 0; k < N; ++k)
                 count++;
 }
 
@@ -15,5 +15,5 @@ int main()
     //test(10);
     //test(100);
     //test(1000);
-    test(100000000);
+    test(1000);
 }
