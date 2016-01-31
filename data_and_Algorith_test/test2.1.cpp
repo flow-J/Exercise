e012659038d7480a15848234dcf073d771e3f18e
flow-J <hongjunjie2012@yeah.net>
2016-02-01 01:31:10 +0800

    .

--- data_and_Algorith_test/test2.1.cpp
+++ data_and_Algorith_test/test2.1.cpp
@@ -3,7 +3,7 @@
 
 auto test(int N)
 {
-    int i, j, k, count = 0;
+    unsigned i, j, k, count = 0;
     for (i = 0; i < N; i++)
         for (j = 0; j < N; j++)
             for (k = 0; k < N; k++)
@@ -15,5 +15,5 @@ int main()
     //test(10);
     //test(100);
     //test(1000);
-    test(1000000000);
+    test(100000000);
 }
