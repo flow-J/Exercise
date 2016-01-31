3ebc8c22a9016418d894b2dd9b0a6a9a990c4ef8
flow-J <hongjunjie2012@yeah.net>
2016-02-01 01:15:05 +0800

    .

---
+++ Algorithm_in_c++/connectivity.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+static const int N = 10000;
+
+int main()
+{
+    int i, j, p, q, id[N], sz[N];
+    for (i = 0; i < N; i++)
+    { id[i] = i; sz[i] = 1; }
+
+    while (cin >> p >> q)
+    {
+        for (i = p; i != id[i]; i = id[i])
+            id[i] = id[id[i]];
+        for (j = q; j != id[j]; j = id[j])
+            id[j] = id[id[j]];
+        if (i == j) continue;
+        if (sz[i] < sz[j])
+            { id[i] = j; sz[j] += sz[i]; }
+        else
+            { id[j] = i; sz[i] += sz[j]; }
+        cout << " " << p << " " << q << endl;
+    }
+}
