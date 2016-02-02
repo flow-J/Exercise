c30de27c6e3e4e04572b4c4e86523297cd5ef389
flow-J <hongjunjie2012@yeah.net>
2016-02-02 16:55:29 +0800

    update

---
+++ Algorithm_in_c++/ex3.6.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+struct poker
+{
+    const string colors[] = {"heart", "spade", "club", "diamond"};
+    const string numers[] = { "3","4","5","6","7","8","9","10","J","Q","K","A","2","BOSS","boss"};
+
+};
