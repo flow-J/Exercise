4fc5f6bc50a7673826265bbfbc922a38b035c556
flow-J <hongjunjie2012@yeah.net>
2016-02-02 17:09:04 +0800

    update

--- Algorithm_in_c++/ex3.6.cpp
+++ Algorithm_in_c++/ex3.6.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <string>
 
+using namespace std;
+/*
 struct poker
 {
     const string colors[] = {"heart", "spade", "club", "diamond"};
     const string numers[] = { "3","4","5","6","7","8","9","10","J","Q","K","A","2","BOSS","boss"};
 
+};
+*/
+
+struct poker
+{
+    typedef string colors;
+    typedef string numbers;
+
+    colors heart, spade, club, diamond;
+    numbers "3", "4", "5", "6", "7", "8", "9", J, Q, K, A, "2", BOSS, boss;
 };
