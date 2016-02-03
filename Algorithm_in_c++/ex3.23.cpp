2d7bb1e92a15a89477d4369a1449199a950a9ef3
flow-J <hongjunjie2012@yeah.net>
2016-02-03 23:41:33 +0800

    update

---
+++ Algorithm_in_c++/ex3.23.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+struct node
+{
+    auto item, node *next;
+    node(int x, node *t)
+    { item = x; next = t; }
+};
+
+typedef node *link;
+
+auto count()
+{
+    while(*next != )
+}
