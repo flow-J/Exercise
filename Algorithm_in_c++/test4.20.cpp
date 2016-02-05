b42a5cead963aa6acf216ba0ef2eeebeae662529
flow-J <hongjunjie2012@yeah.net>
2016-02-06 00:44:27 +0800

    ./FIFO.hpp:8:5: error: C++ requires a type specifier for all declarations

---
+++ Algorithm_in_c++/test4.20.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "FIFO.hpp"
+#include <stdlib.h>
+
+static const int M = 4;
+
+int main(int argc, char *argv[])
+{
+    int N = atoi(argv[1]);
+    QUEUE<int> queues[M];
+}
