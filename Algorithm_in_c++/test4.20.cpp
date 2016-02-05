f61c634bbdab8eb3154eea266915973f9f811d68
flow-J <hongjunjie2012@yeah.net>
2016-02-06 01:04:10 +0800

    .

--- Algorithm_in_c++/test4.20.cpp
+++ Algorithm_in_c++/test4.20.cpp
@@ -2,10 +2,27 @@
 #include "FIFO.hpp"
 #include <stdlib.h>
 
+using namespace std;
 static const int M = 4;
 
 int main(int argc, char *argv[])
 {
     int N = atoi(argv[1]);
     QUEUE<int> queues[M];
+    for (int i = 0; i < N; i++, cout << endl)
+    {
+        int in = rand() % M, out = rand() % M;
+        queues[in].put(i);
+        cout << i << " in ";
+        if (!queues[out].empty())
+            cout << queues[out].get() << " out";
+        cout << endl;
+        for (int k = 0; k < M; k++, cout << endl)
+        {
+            QUEUE<int> q = queues[k];
+            cout << k << ": ";
+            while (!q.empty())
+                cout << q.get() << " ";
+        }
+    }
 }
