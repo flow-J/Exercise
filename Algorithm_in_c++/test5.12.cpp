dca2891bbebee72bed18408782b188709b172569
flow-J <hongjunjie2012@yeah.net>
2016-02-06 03:53:01 +0800

    x

---
+++ Algorithm_in_c++/test5.12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+struct Item {int size; int val; };
+
+
+int knap(int cap)
+{
+    int i, space, max, t;
+    for (i = 0; max = 0; i < N; i++)
+        if ((space = cap-items[i].size) >= 0)
+            if ((t = knap(space) + items[i].val) > max)
+                max = t;
+    return max;
+}
