e8a27b6b08e5225aba445a3aa760aac1cfc5cc9c
flow-J <hongjunjie2012@yeah.net>
2016-02-21 02:37:30 +0800

    .

---
+++ CLRS/ch12/tree.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+struct node
+{
+    key;
+    left;
+    right;
+};
+
+class tree
+{
+public:
+    tree_walk(val)
+    {
+        if (val != nullptr)
+        {
+            inorder(val,left);
+            cout << val.key << endl;
+            inorder(val,right);
+        }
+    }
+
+    inorder()
+    {}
+
+    search(tree, k)
+    {
+        if (tree == nullptr || k == tree.key)
+        {
+            return tree;
+        }
+        if (k < tree)
+    }
+
+private:
+        
+};
+
+inline operator <<(ostream& os, val)
+{
+    std::cout << val.
+}
