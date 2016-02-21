674f4ec30f5d2d2f9b6d229a805bef07c763600d
flow <hongjunjie2012@yeah.net>
2016-02-21 21:58:30 +0800

    Update main.cpp

--- CLRS/ch12/main.cpp
+++ CLRS/ch12/main.cpp
@@ -1,1 +1,22 @@
-#include <>
+#include <iostream>
+#include <string>
+#include "node.cpp"
+#include "binary_search_tree.cpp"
+
+int main()
+{
+  binary_search_tree<int, std::string> tree;
+
+  tree.insert_recur(5,"005");
+    tree.insert_recur(611,"611");
+    tree.insert_recur(6,"006");
+    tree.insert_recur(16,"016");
+
+    tree.remove(6);
+
+    tree.inorder_print();
+
+    std::cout << "end" << std::endl;
+    return 0;
+  
+}
