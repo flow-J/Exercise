8ae8f160e4529dd58af198272cbdcbd4bb0df4b9
flow-J <hongjunjie2012@yeah.net>
2016-03-09 11:30:13 +0800

    python learning

---
+++ python/flasky/ex3.py
@@ -0,0 +1,10 @@
+from flask import request
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
+
+
+if __name__ == '__main__':
+    app.run()
