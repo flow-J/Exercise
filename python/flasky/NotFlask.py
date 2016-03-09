a7dc3365fc3cc0450efd6b200532cbccd95b16dd
flow-J <hongjunjie2012@yeah.net>
2016-03-09 16:01:15 +0800

    bug

--- python/flasky/NotFlask.py
+++ python/flasky/NotFlask.py
@@ -1,17 +1,41 @@
 import re
 
-route_regex = re.compile("^/hello/(.+)$")
-match = route_regex.match("/hello/ains")
+class NotFlask():
+    def __init__(self):
+        self.routes = []
 
-#print match.groups()
+    @staticmethod
+    def build_route_pattern(self, route):
+        route_regex = re.sub(r'(<\w+>)', r'(?p\1.+)', route)
+        return re.compile("^{}$".format(route_regex))
 
-route_regex = re.compile(r'^/hello/(?P<username>.+)$')
-match = route_regex.match("/hello/ains")
+    def route(self, route_str):
+        def decorator(self,f):
+            route_pattern = self.build_route_pattern(route_str)
+            self.routes.append((route_pattern, f))
+            return f
+        return decorator
 
-#print match.groupdict()
+    def get_route_match(self, path):
+        for route_pattern, view_function in self.routes:
+            m = route_pattern.match(path)
+            if m:
+                return m.groupdict(), view_function
 
-def build_route_pattern(route):
-    route_regex = re.sub(r'(<\w+>)', r'(?P\1.+)', route)
-    return re.compile("^{}$".format(route_regex))
+        return None
 
-print build_route_pattern('/hello/<username>')
+    def serve(self, path):
+        route_match = self.get_route_match(path)
+        if route_match:
+            kwargs, view_function = route_match
+            return view_function(**kwargs)
+        else:
+            raise ValueError('Route "{}" has not been registered'.format(path))
+
+app = NotFlask()
+
+@app.route("/hello/<username>")
+def hello_user(username):
+    return "Hello {}!".format(username)
+
+print app.serve("/hello/ains")
