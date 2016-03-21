2cad3a4b0d5e56a7a6873ed5cce0c99cb9e17e77
flow-J <hongjunjie2012@yeah.net>
2016-03-21 13:24:20 +0800

    bug

--- python/urllib2_test/qiushibaike.py
+++ python/urllib2_test/qiushibaike.py
@@ -1,15 +1,26 @@
 # coding:utf-8
 import urllib
 import urllib2
+import re
 
 page = 1
 url = 'http://www.qiushibaike.com/hot/page/' + str(page)
 user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 headers = {'User-Agent' : user_agent }
+
+
+
 try:
     request = urllib2.Request(url, headers = headers)
     response = urllib2.urlopen(request)
-    print response.read()
+    content = response.read().decode('utf-8')
+    pattern = re.compile('<div.*?author>.*?<a.*?<img.*?>(.*?)</a>.*?<div.*?'+'content">(.*?)<!--(.*?)-->.*?</div>(.*?)<div class="stats.*?class=number">(.*?)</i>',re.S)
+
+    items = re.findall(pattern, content)
+    for item in items:
+        haveImg = re.search("img", item[3])
+        if not haveImg:
+            print item[0], item[1], item[2],item[4]
 except urllib2.URLError, e:
     if hasattr(e, "code"):
         print e.code
