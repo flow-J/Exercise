84c955d995e85fccc2e6af9b2e1e6e4dbd6541e3
flow-J <hongjunjie2012@yeah.net>
2016-03-21 12:58:29 +0800

    fixbug

--- python/urllib2_test/qiushibaike.py
+++ python/urllib2_test/qiushibaike.py
@@ -4,8 +4,10 @@ import urllib2
 
 page = 1
 url = 'http://www.qiushibaike.com/hot/page/' + str(page)
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+headers = {'User-Agent' : user_agent }
 try:
-    request = urllib2.Request(url)
+    request = urllib2.Request(url, headers = headers)
     response = urllib2.urlopen(request)
     print response.read()
 except urllib2.URLError, e:
