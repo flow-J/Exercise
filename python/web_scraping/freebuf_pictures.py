b2a2fec720a67bc34344cfa5baf0971bd7101a6b
flow <hongjunjie2012@yeah.net>
2016-03-24 19:23:01 +0800

    Update freebuf_pictures.py

--- python/web_scraping/freebuf_pictures.py
+++ python/web_scraping/freebuf_pictures.py
@@ -1,15 +1,17 @@
+#-*-coding:utf8-*-
+import sys
+
+reload(sys) 
+sys.setdefaultencoding('utf-8')
 import urllib
 from bs4 import BeautifulSoup
 import re
 url = 'http://shop.freebuf.com/'
 print "prepare&reading to read theweb"
 data = urllib.urlopen(url).read()
-soup = BeautifulSoup(data)
-
-print soup
-
-itemlist = soup.findAll(name='div', attrs={'class':'col-sm-6 col-md-4 col-lg-4 mail-product-list'})
+soup = BeautifulSoup(data, "html5lib")
+#<div class="col-sm-6 col-md-4col-lg-4 mall-product-list">
+itemlist = soup.findAll(name='div',attrs={'class':'col-sm-6 col-md-4 col-lg-4 mall-product-list'})
 print itemlist
 for item in itemlist:
-#    urllib.urlretrieve(url=item.img['src'], filename=item.img['alt']+item.img['src'][-4:])
-    print item.img['src'][-4:]
+	print item.img['src'][-4:]
