e182b745b053ef0f31dbb74dff571e64ebef038f
flow-J <hongjunjie2012@yeah.net>
2016-03-28 18:13:56 +0800

    some

--- python/flasky/hello.py
+++ python/flasky/hello.py
@@ -19,7 +19,8 @@ def time():
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('index.html',
+            time = datetime.utcnow())
 
 @app.route('/base/')
 def base():
@@ -28,6 +29,10 @@ def base():
 @app.errorhandler(404)
 def page_not_found(justaword):
     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def server_error(justaword):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(host= '127.0.0.1', port = 7777)
