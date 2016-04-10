124a98a657efc3a4fd2a67d32eda486ae05e421e
flow-J <hongjunjie2012@yeah.net>
2016-04-11 06:08:47 +0800

    .

--- python/flasky/manage.py
+++ python/flasky/manage.py
@@ -12,7 +12,8 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import os
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
-from flask.ext.mail import Mail
+from flask.ext.mail import Mail, Message
+from threading import Thread
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -37,9 +38,24 @@ app.config['MAIL_PORT'] = 25
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[FLASKY]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 mail = Mail(app)# 这个必须在config下面　不然就得不到config的东西
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                    sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 #***************************** database **********************************#
 class Role(db.Model):
@@ -119,6 +135,9 @@ def hello():
             user = User(username = form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User',
+                        'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data   #IF TRUE后把data里的值赋给name
@@ -155,4 +174,5 @@ def email():
 if __name__ == '__main__':
     print os.environ.get('MAIL_USERNAME')
     print os.environ.get('MAIL_PASSWORD')
-    manager.run()
+    print os.environ.get('FLASKY_ADMIN')
+    app.run(debug=True)
