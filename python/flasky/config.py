1649bd5f395ab119e553f5988bbdf2695ba24972
flow-J <hongjunjie2012@yeah.net>
2016-04-11 07:25:02 +0800

    config

---
+++ python/flasky/config.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    FLASKY_MAIL_SUBJECT_PREFIX = '[FLASKY]'
+    FLASKY_MAIL_SENDER = 'hongjunjie2012 <hongjunjie2012@yeah.net>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config): # 开发模式的配置
+    DEBUG = True
+    MAIL_SERVER = ''
+    MAIL_PORT =
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or\
+            'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class TestingConfig(Config): # 测试配置
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or\
+            'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or\
+            'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+config = {
+        'development': DevelopmentConfig,
+        'testing': TestingConfig,
+        'production': ProductionConfig,
+
+        'default': DevelopmentConfig
+}
