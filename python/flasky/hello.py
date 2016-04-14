c3592baa4840bd8896524c87c9b68d03ad383892
flow-J <hongjunjie2012@yeah.net>
2016-04-14 13:43:28 +0800

    .

--- python/flasky/hello.py
+++ python/flasky/hello.py
@@ -19,7 +19,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+        'sqlite:///' + os.path.join(basedir, 'database.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 db = SQLAlchemy(app)
