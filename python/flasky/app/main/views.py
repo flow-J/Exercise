c00cf6cb0ab94f4e79c9c0a55ca1166425b676f9
flow-J <hongjunjie2012@yeah.net>
2016-05-25 16:26:44 +0800

    解决了user.html的排版问题

--- python/flasky/app/main/views.py
+++ python/flasky/app/main/views.py
@@ -11,22 +11,15 @@ from ..decorators import admin_required
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username = form.name.data)
-            db.session.add(user)
-            session['known'] = False
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        form.name.data = ''
+    form = PostForm()
+    if current_user.can(Permission.WRITE_ARTICLES) and \
+            form.validate_on_submit():
+        post = Post(body=form.body.data,
+                    author=current_user._get_current_object())
+        db.session.add(post)
         return redirect(url_for('.index'))
-    return render_template('index.html',
-            form=form, name=session.get('name'),
-            known=session.get('known', False),
-            current_time=datetime.utcnow())
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts=posts, current_time=datetime.utcnow())
 
 @main.route('/user/<username>')
 def user(username):
