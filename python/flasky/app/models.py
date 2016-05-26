55c8da4e4b81c60e2d00cae4a015d0a223735e82
flow-J <hongjunjie2012@yeah.net>
2016-05-26 18:38:30 +0800

    增加了随机博客文章

--- python/flasky/app/models.py
+++ python/flasky/app/models.py
@@ -57,6 +57,21 @@ class Post(db.Model):
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 
+    @staticmethod
+    def generate_fake(count=100):
+        from random import seed, randint
+        import forgery_py
+
+        seed()
+        user_count = User.query.count()
+        for i in range(count):
+            u = User.query.offset(randint(0, user_count -1)).first()
+            p = Post(body=forgery_py.lorem_ipsum.sentences(randint(1,3)),
+                        timestamp=forgery_py.date.date(True),
+                        author=u)
+            db.session.add(p)
+            db.session.commit()
+
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -167,6 +182,30 @@ class User(db.Model, UserMixin):
 
     def is_administrator(self):
         return self.can(Permission.ADMINISTER)
+
+###################### 生产虚拟用户和博客文章 ##########################
+    @staticmethod
+    def generate_fake(count=100):
+        from sqlalchemy.exc import IntegrityError
+        from random import seed
+        import forgery_py
+
+        seed()
+        for i in range(count):
+            u = User(email=forgery_py.internet.email_address(),
+                     username=forgery_py.internet.user_name(True),
+                     password=forgery_py.lorem_ipsum.word(),
+                     confirmed=True,
+                     name=forgery_py.name.full_name(),
+                     location=forgery_py.address.city(),
+                     about_me=forgery_py.lorem_ipsum.sentence(),
+                     member_since=forgery_py.date.date(True))
+            db.session.add(u)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
+
 
 class AnonymousUser(AnonymousUserMixin):
     def can(self, permissions):
--- python/flasky/manage.py
+++ python/flasky/manage.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import os
 from app import create_app, db
-from app.models import User, Role
+from app.models import User, Role, Permission, Post
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
@@ -10,7 +10,7 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)# dict是字典函数　将序列转换成字典
+    return dict(app=app, db=db, User=User, Role=Role, Permission=Permission, Post=Post)# dict是字典函数　将序列转换成字典
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
