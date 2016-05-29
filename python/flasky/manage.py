e715ac01070c2253b45ec77ffa0c09abac5e1d4d
flow-J <hongjunjie2012@yeah.net>
2016-05-29 17:43:50 +0800

    bla

--- python/flasky/manage.py
+++ python/flasky/manage.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import os
 from app import create_app, db
-from app.models import User, Role, Permission, Post
+from app.models import User, Follow, Role, Permission, Post
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
@@ -10,7 +10,7 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role, Permission=Permission, Post=Post)# dict是字典函数　将序列转换成字典
+    return dict(app=app, db=db, User=User, Follow=Follow, Role=Role, Permission=Permission, Post=Post)# dict是字典函数　将序列转换成字典
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
